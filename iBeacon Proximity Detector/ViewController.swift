322f23afd28826154cc90947ea1c55b9002a3481
artnest <artnest789@gmail.com>
2020-06-11 18:44:21 +0300

    Initial Commit

---
+++ iBeacon Proximity Detector/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iBeacon Proximity Detector
+//
+//  Created by Artyom Nesterenko on 11/6/20.
+//  Copyright © 2020 artnest. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
